06ab1d6f2f74c025c5e3d9928fd305d569766da7
tamagram <tamasupaka@gmail.com>
2023-02-26 01:05:41 +0900

    Prevent XSS attack

--- comment.php
+++ comment.php
@@ -3,7 +3,8 @@ $json = file_get_contents('./comments.json');
 $comments = json_decode($json);
 if (isset($_POST['comment'])) {
   var_dump($_POST['comment']);
-  array_push($comments, $_POST['comment']);
+  $sanitized = htmlspecialchars($_POST['comment']);
+  array_push($comments, $sanitized);
   file_put_contents('./comments.json', json_encode($comments));
   header("Location: index.php");
   exit;
