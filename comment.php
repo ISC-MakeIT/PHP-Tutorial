1291f56a699b9909cdf220a74685d6198b7c5850
tamagram <tamasupaka@gmail.com>
2023-02-27 01:24:48 +0900

    Update docs

--- comment.php
+++ comment.php
@@ -1,7 +1,8 @@
 <?php
 $pdo = require_once 'connect.php';
 if (isset($_POST['comment'])) {
-  $sql = "INSERT INTO comments (user_id, body) VALUES (1, '{$_POST['comment']}')";
+  $sanitized  = htmlspecialchars($_POST['comment']);
+  $sql = "INSERT INTO comments (user_id, body) VALUES (1, '{$sanitized}')";
   var_dump($sql);
   $statement = $pdo->query($sql);
   header("Location: index.php");
